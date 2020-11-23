39cf7917c3fdbb219b102fa76645ea607600555d
user01 <xl00127@yahoo.co.jp>
2020-11-23 13:22:22 +0900

    initial commit

---
+++ app/src/main/java/jp/co/mlzvite/mustdo/MainActivity.java
@@ -0,0 +1,34 @@
+package jp.co.mlzvite.mustdo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Splash screen show time in 3 seconds.
+     */
+    private static final short SPLASH_TIME = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Hide status bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+    }
+}
